f1caf934ef48c58bb728f0190fa963093115d75d
jorjoh <jorgen-johansen@outlook.com>
2018-08-28 19:26:42 +0200

    Sende metoder mellom komponenter

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import person from './Person/Person';
 
 class App extends Component {
   state = {
-    Persons: [
+    persons: [
       { name: 'Jørgen', age: 25},
       { name: 'Kari', age: 29},
       { name: 'Stella', age: 28}
-    ] 
+    ],
+    
+    otherState: 'some other state'
   }
   
   switchNameHandler = () => {
     console.log('Was clicked');
+   // Dont do this this.state.Persons[0].name = "Johansen"
+   this.setState( {
+     persons: [
+        { name: 'Johansen', age: 25},
+        { name: 'Kari', age: 29},
+        { name: 'Dog', age: 29}
+     ]
+   } )
   }
 
   render() {
@@ -22,9 +32,15 @@ class App extends Component {
        <h1>Hi, i'm a react app </h1>
        <p>This is working</p>
       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.Persons[0].name} age={this.state.Persons[0].age}/>
-       <Person name={this.state.Persons[1].name} age={this.state.Persons[1].age}/>
-       <Person name={this.state.Persons[2].name} age={this.state.Persons[2].age}/>
+       <Person 
+       name={this.state.persons[0].name} 
+       age={this.state.persons[0].age}/>
+       <Person 
+       name={this.state.persons[1].name} 
+       age={this.state.persons[1].age} click={this.switchNameHandler}> My hobbies: Football </Person>
+       <Person 
+       name={this.state.persons[2].name} 
+       age={this.state.persons[2].age}/>
       </div>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'dose this work now?'));
